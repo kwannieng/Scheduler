1bd7d06aa0a5a055f07cbaeb8e8a271125ca5210
kwannieng <78864242+kwannieng@users.noreply.github.com>
2023-01-29 21:57:16 +0000

    Completed Save Event function

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,16 +25,12 @@ let scheduleTime = [
   $('#4pm').text(), 
   $('#5pm').text() ]
 
-  let button = $(".saveBtn")
-
-  console.log(button)
-
 
 displayDate()
 function displayDate() {
     let rightNow = moment().format('dddd, MMMM Do YYYY');
     todayDate.text(rightNow);
-  }
+}
 
 colorTimeblocks ()
 function colorTimeblocks (){
@@ -56,129 +52,29 @@ function colorTimeblocks (){
   }
 }
 
-// let saveButtons = document.querySelector("button")
-// let myEvent = document.querySelector(".description")
-// let myEvents = []
 
-// function renderEvent() {
-//   // Clear todoList element and update todoCountSpan
-//   // myEvent.innerHTML = "";
-  
-//   // Render a new p for each timeblock
-//   for (var b = 0; b < myEvents.length; b++) {
-//     let schedule = myEvent[b];
 
-//     let p = document.createElement("p");
-//     p.textContent = schedule;
-//     p.setAttribute("data-index", b);
-//     myEvent.appendChild(p);
-//   }
-// }
+$(".saveBtn").on("click", function(){
 
-// function init() {
-//   // Get stored todos from localStorage
-//   // Parsing the JSON string to an object
-//   let stored = JSON.parse(localStorage.getItem("Event"));
+  let time = $(this).siblings(".hour").children().attr("id");
+  let myEvent = $(this).siblings(".description").val();
+  localStorage.setItem(time,myEvent);
 
-//   // If my Event were retrieved from localStorage, update the myEvents array to it
-//   if (stored !== null) {
-//     myEvents = stored;
-//   }
+})
 
-//   // Render todos to the DOM
-//   renderEvent();
-// }
 
-// function storeEvent() {
-//   // Stringify and set "todos" key in localStorage to todos array
-//   localStorage.setItem("Event", JSON.stringify(myEvents));
-// }
-
-// // When save button is clicked...
-// for (a=0; a<saveButtons.length; a++){
-
-// saveButtons.addEventListener("click", function(e) {
-//   e.preventDefault();
-//   let eventText = myEvent.value.trim();
-
-//   // Return from function early if submitted todoText is blank
-//   if (eventText === "") {
-//     return;
-//   }
-
-//   // Add new event to myEvents array, clear the input
-//   myEvents.push(eventText);
-//   myEvent.value = "";
-
-//   // Store updated todos in localStorage, re-render the list
-//   storeEvent();
-//   renderEvent();
-
-// })
-
-// }
+$("#textbox1").val(localStorage.getItem("9am"))
+$("#textbox2").val(localStorage.getItem("10am"))
+$("#textbox3").val(localStorage.getItem("11am"))
+$("#textbox4").val(localStorage.getItem("12nn"))
+$("#textbox5").val(localStorage.getItem("1pm"))
+$("#textbox6").val(localStorage.getItem("2pm"))
+$("#textbox7").val(localStorage.getItem("3pm"))
+$("#textbox8").val(localStorage.getItem("4pm"))
+$("#textbox8").val(localStorage.getItem("5pm"))
 
 
 
-var myEvent = document.querySelector(".description");
-var myEvents = [];
 
-init();
-
-function renderEvents() {
-  
-  // Render a new p for each timeblock
-  for (var a = 0; a < myEvents.length; a++) {
-    let schedule = myEvents[i];
-
-    let p = document.createElement("p");
-    p.textContent = schedule;
-    p.setAttribute("data-index", a);
-
-    myEvent.appendChild(p);
-  }
-}
-
-function init() {
-  // Get stored todos from localStorage
-  // Parsing the JSON string to an object
-  var storeEvents = JSON.parse(localStorage.getItem("myEvents"));
-
-  // If myEvents were retrieved from localStorage, update the myEvents array to it
-  if (storeEvents !== null) {
-    myEvents = storeEvents;
-  }
-
-  // Render myEvents to the DOM
-  renderEvents();
-}
-
-
-function store() {
-
-  // Stringify and set "myEvents" key in localStorage to myEvents array
-  localStorage.setItem("myEvents", JSON.stringify(myEvents));
-}  
-// When button is clicked...
-
-for (var b = 0; b < button.length; b++) {
-  button.on("click", function(e) {
-  e.preventDefault();
-
-  let eventText= myEvent.value.trim();
-
-  // Return from function early if submitted eventTextis blank
-  if (eventText=== "") {
-    return;
-  }
-
-  // Add new eventText to myEvents array, clear the input
-  myEvents.push(eventText);
-  myEvent.value = "";
-  })
-  // Store updated myEvents in localStorage, re-render the list
-  store();
-  renderEvents();
-}
 
 })
